4694cbf724019ff98697c0d8c5226a2d29190e8b
Zinhle Mahlangu <129791680+ST10375898@users.noreply.github.com>
2025-07-30 21:32:35 +0200

    final push

---
+++ src/components/SpinnerObj.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Spinner } from "flowbite-react";
+
+const SpinnerObj = () => {
+  return (
+    <Spinner size="lg" aria-label="Default status example" />
+  )
+}
+
+export default SpinnerObj
